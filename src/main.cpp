cd27b2a18529a5e0e393b6b4842217e644a0172b
Tim Kingsmill <tim.kingsmill@gmail.com>
2024-04-14 14:59:08 +0930

    Version 1.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,55 @@
 #include <Arduino.h>
-#include <BluetoothA2DPSink.h>
-#include <FastLED.h>
+#include "BluetoothA2DPSinkQueued.h"
 
-#define NUM_LEDS 1
-#define LED_DATA_PIN 2
+// https://github.com/pschatzmann/ESP32-A2DP/wiki/External-DAC
 
-CRGB leds[NUM_LEDS]; 
+BluetoothA2DPSinkQueued a2dp_sink;
 
-I2SStream out;
-BluetoothA2DPSink a2dp_sink(out);
+// ------------------------------------------------------------------------------
+// I2S Output Connection Pins
+#define I2S_DOUT       22
+#define I2S_LRC        25
+#define I2S_BCLK       26
+
+// Internal DAC Output Pins
+#define DAC_1_OUT      25
+#define DAC_2_OUT      26
+
+// ------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 
 void setup() 
 {
-    FastLED.addLeds<WS2812, LED_DATA_PIN, GRB>(leds, NUM_LEDS);
-    FastLED.setBrightness(30);
+    static i2s_config_t i2s_config = 
+    {
+        .mode = (i2s_mode_t) (I2S_MODE_MASTER | I2S_MODE_TX),
+        .sample_rate = 44100, // Corrected by info from bluetooth.
+        .bits_per_sample = (i2s_bits_per_sample_t)32,
+        .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+        .communication_format = (i2s_comm_format_t) (I2S_COMM_FORMAT_STAND_I2S),
+        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1, // default interrupt priority
+        .dma_buf_count = 16, // Tim, Was 8
+        .dma_buf_len = 256,  // Tim, Was 64
+        .use_apll = true, // Tim, this seems reduce noice.
+        .tx_desc_auto_clear = true, // Tim K: Turned off to reduce popping noise     
+    };  
 
-    a2dp_sink.start("MyMusic");  
+    a2dp_sink.set_i2s_config(i2s_config);  
+
+    a2dp_sink.set_i2s_ringbuffer_size(2 * 32 * 1024);
+    
+    
+    a2dp_sink.set_auto_reconnect(true);
+    a2dp_sink.start("Tims-WRover-Speakers");
 }
 
-void loop() 
-{
-    leds[0] = CRGB::Red;
-    FastLED.show();
-    delay(1000);
-    leds[0] = CRGB::Green;
-    FastLED.show();
-    delay(1000);
-    leds[0] = CRGB::Blue;
-    FastLED.show();
-    delay(1000);
+// ------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
+
+void loop() {
+    delay(10000);
 }
+
+// ------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
+
